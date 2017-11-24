c000eab8e58d0fe06d4f595a80f1fd5ad503304c
13981779464 <32384610+13981779464@users.noreply.github.com>
2017-11-24 16:52:00 +0800

    Add files via upload

--- a.php
+++ a.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-type: text/html; charset=utf-8");
+/* header("Content-type: text/html; charset=utf-8"); */
 $a=mysqli_connect('localhost', 'root', 'root', 'yi');
 $sql="SELECT * FROM all_a " ;
 $b = mysqli_query($a,$sql);
