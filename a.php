8bbe2400301bd9548658fc0c9c6a5233f0fd41dc
13981779464 <32384610+13981779464@users.noreply.github.com>
2017-11-24 15:14:53 +0800

    Add files via upload

---
+++ a.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+$a=mysqli_connect('localhost', 'root', 'root', 'yi');
+$sql="SELECT * FROM all_a " ;
+$b = mysqli_query($a,$sql);
+while ( $re = mysqli_fetch_assoc($b)) {
+     echo $re['name']."<br/>";
+}
+$x=$_POST['value1'];
+echo$x;
+// if ($x) {
+//     echo  json_encode($re);
+// }
